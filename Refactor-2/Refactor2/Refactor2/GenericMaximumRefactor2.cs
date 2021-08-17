b3aaec7a4ddb3ad2d140ab62c49256931fc3e56a
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-17 17:34:28 +0530

    Refactor2

---
+++ Refactor-2/Refactor2/Refactor2/GenericMaximumRefactor2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor2
+{
+    class GenericMaximumRefactor2<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+        public GenericMaximumRefactor2(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0) 
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            return default;
+        }
+    }
+}
---
+++ Refactor-2/Refactor2/Refactor2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Refactor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IntegercheckNumber = GenericMaximumRefactor2<int>.MaxValue(10, 20, 100);
+            Console.WriteLine("The Largest Of Three Integer Number Is: "+IntegercheckNumber);
+            double FloatCheckNumber = GenericMaximumRefactor2<double>.MaxValue(20.3, 30.5, 50.6);
+            Console.WriteLine("The Largest Of Three Float Number Is: "+FloatCheckNumber);
+            string StringCheckNumber = GenericMaximumRefactor2<string>.MaxValue("1000", "2000", "4000");
+            Console.WriteLine("The Largest Of Three String Number: "+StringCheckNumber);
+        }
+    }
+}

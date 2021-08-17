04a5a158efb7bea611c176616cdc5555ccbc4af6
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-17 17:27:48 +0530

    Refactor1

---
+++ Refactor-1/Refactor1/Refactor1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Refactor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int checkIntNumber = GenericsRefactor1.IntegerMaximumNumber(50, 60, 70);
+            Console.WriteLine("The Largest Of Three Integer Number Is: "+checkIntNumber);
+            double checkFloatNumber = GenericsRefactor1.FloatMaximumNumber(20.5, 30.6, 40.7);
+            Console.WriteLine("The Largest Of Three Float Number Is: "+checkFloatNumber);
+            string checkSringNumber = GenericsRefactor1.StringMaximumNumber("1000", "5000", "10000");
+        }
+    }
+}

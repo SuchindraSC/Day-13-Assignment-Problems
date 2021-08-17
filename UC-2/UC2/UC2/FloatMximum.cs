bc001cc761a0e9b3315cfb39594a4bbb342a7689
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-17 17:20:55 +0530

    UC2

---
+++ UC-2/UC2/UC2/FloatMximum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2
+{
+    class FloatMximum
+    {
+        public static Double FloatMximumNumber(double firstValue, double secondValue, double thirdValue)
+        {
+            if(firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if(secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+
+            if(thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            throw new Exception("First Value, Second Value & Third Value Are Same");
+        }
+    }
+}
---
+++ UC-2/UC2/UC2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum Of 3 Float Values");
+
+            double checkNumber = FloatMximum.FloatMximumNumber(50.3, 50.6, 50.8);
+            Console.WriteLine("\nThe Largest Of 3 Float Number Is "+checkNumber);
+        }
+    }
+}

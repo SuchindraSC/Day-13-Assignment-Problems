655af42597ce5085a9baead606063465d4e2d257
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-17 17:25:13 +0530

    UC3

---
+++ UC-3/UC3/UC3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding Maximum Of Three String");
+
+            string checkNumber = StringMaximum.StringMaximumValue("50", "60", "70");
+            Console.WriteLine("\nThe Largest Of Three Number Is: "+checkNumber);
+        }
+    }
+}
---
+++ UC-3/UC3/UC3/StringMaximum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3
+{
+    class StringMaximum
+    {
+        public static string StringMaximumValue(string firstValue, string secondValue, string thirdValue)
+        {
+            if(firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if(secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+
+            if(thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(thirdValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            throw new Exception("First Value, Second Value & Third Value Are Same");
+        }
+    }
+}

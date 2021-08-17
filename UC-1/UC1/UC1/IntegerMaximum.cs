e96ba944237d57a106f8d0173603c37e7e562b9f
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-17 16:27:13 +0530

    Day 13 Assignment Problem

---
+++ UC-1/UC1/UC1/IntegerMaximum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1
+{
+    class IntegerMaximum
+    {
+        public static int IntegerMaximumNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            throw new Exception("First Value, Second Value & Third Value Are Same");
+        }
+    }
+}
---
+++ UC-1/UC1/UC1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding Integer Maximum Number");
+
+            int CheckNumber1 = IntegerMaximum.IntegerMaximumNumber(70, 50, 60);
+            Console.WriteLine("\nLargest of The Three Number Is: "+CheckNumber1);
+
+            int CheckNumber2 = IntegerMaximum.IntegerMaximumNumber(100, 500, 300);
+            Console.WriteLine("\nLargest of The Three Number Is: " + CheckNumber2);
+
+            int CheckNumber3 = IntegerMaximum.IntegerMaximumNumber(1000, 3000, 7000);
+            Console.WriteLine("\nLargest of The Three Number Is: " + CheckNumber3);
+        }
+    }
+}

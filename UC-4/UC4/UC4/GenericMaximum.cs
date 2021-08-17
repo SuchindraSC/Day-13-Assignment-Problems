9331b0dd228afc83a7f00eb000d6106cf0ef1b3a
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-17 17:37:35 +0530

    UC4

---
+++ UC-4/UC4/UC4/GenericMaximum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue, fourthValue;
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue, T fourthValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+            this.fourthValue = fourthValue;
+        }
+
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue, T fourthValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0 && firstValue.CompareTo(fourthValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0 && secondValue.CompareTo(fourthValue) >= 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0 && thirdValue.CompareTo(fourthValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            if (fourthValue.CompareTo(firstValue) >= 0 && fourthValue.CompareTo(secondValue) >= 0 && fourthValue.CompareTo(thirdValue) >= 0)
+            {
+                return fourthValue;
+            }
+
+            return default;
+        }
+    }
+}
---
+++ UC-4/UC4/UC4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int IntegercheckNumber = GenericMaximum<int>.MaxValue(10, 20, 100, 400);
+            Console.WriteLine("The Largest Of Three Integer Number Is: " + IntegercheckNumber);
+            double FloatCheckNumber = GenericMaximum<double>.MaxValue(20.3, 30.5, 50.6, 65.95);
+            Console.WriteLine("The Largest Of Three Float Number Is: " + FloatCheckNumber);
+            string StringCheckNumber = GenericMaximum<string>.MaxValue("1000", "2000", "4000", "8000");
+            Console.WriteLine("The Largest Of Three String Number: " + StringCheckNumber);
+        }
+    }
+}
